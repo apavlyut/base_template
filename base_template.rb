8a8775fbaa190871b32c0c5d1de2143a9b7a7e79
Alexander Pavlyut <ap@Yabloid.local>
2010-04-04 15:52:42 +0400

    Added some changes

--- base_template.rb
+++ base_template.rb
@@ -1,1 +1,40 @@
-run "echo TODO > README"
+if yes? "Copy base templates?"  
+  run "cp /Users/ap/code/app_template/layouts/application.html.erb app/views/layouts/"
+  run "cp /Users/ap/code/app_template/helpers/layout_helper.rb app/helpers/"
+  run "cp /Users/ap/code/app_template/stylesheets/* public/stylesheets/"
+end
+
+if yes? "Use authentication and authorization?"
+  gem "authlogic"
+  gem "declarative_authorization", :source => "http://gemcutter.org"
+end
+
+if yes? "Use russian pack?"
+  gem "russian"
+  gem "rutils"
+end
+
+if yes? "Need search functionality?"
+  gem "searchlogic"
+end
+
+rake "gems:install", :sudo => true
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+generate :controller, "welcome index"
+route "map.root :controller => 'welcome'"
+run "rm public/index.html"
+git :rm => "public/index.html"
+
+git :init
+git :add => ".", :commit => "-m 'initial commit'"
